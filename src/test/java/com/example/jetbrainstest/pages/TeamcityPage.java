8f6336c12e7dfd10dd160f59a71f6590af02dc25
Dochkin-sergey <96723560+Dochkin-sergey@users.noreply.github.com>
2025-03-17 21:37:28 +0300

    NewTestDochkin

---
+++ src/test/java/com/example/jetbrainstest/pages/TeamcityPage.java
@@ -0,0 +1,35 @@
+package com.example.jetbrainstest.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static com.example.jetbrainstest.tests.BaseTest.getDriver;
+
+public class TeamcityPage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//*[contains(text(), 'Get started free')]")
+    private WebElement ButtonStartForFree;
+    @FindBy(css = "[data-test=\"site-header-profile-action\"]")
+    private WebElement AuthorizationIcon;
+
+    public boolean ButtonActivityGetStartedFree (){
+        return ButtonStartForFree.isEnabled();
+    }
+
+    public void clickAuthorizationIcon(){
+        AuthorizationIcon.click();
+    }
+
+    public String URLAuthorization (String URL) {
+        return driver.getCurrentUrl();
+    }
+
+    public TeamcityPage (WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+}
---
+++ src/test/java/com/example/jetbrainstest/tests/TeamcityTest.java
@@ -0,0 +1,47 @@
+package com.example.jetbrainstest.tests;
+
+import com.example.jetbrainstest.MyExtension;
+import com.example.jetbrainstest.pages.AppCodePage;
+import com.example.jetbrainstest.pages.TeamcityPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MyExtension.class)
+public class TeamcityTest extends BaseTest {
+
+
+    private TeamcityPage TeamcityPage;
+    @BeforeEach
+    @Override
+    public void setUp(){
+        super.setUp();
+        getDriver().get("https://www.jetbrains.com/teamcity/");
+        TeamcityPage TeamcityPage = new TeamcityPage(getDriver());
+    }
+
+    @Test
+    @DisplayName("Проверка, что кнопка Get started free активна")
+    public void buttonGetStartedFreeActiv(){
+        assertTrue(TeamcityPage.ButtonActivityGetStartedFree(), "Кнопка не активна");
+    }
+
+    @Test
+    @DisplayName("Переход на страницу авторизации по иконке")
+    public void SwitchoAuthorization(){
+        TeamcityPage.clickAuthorizationIcon();
+        assertEquals(getDriver().getCurrentUrl(), "https://account.jetbrains.com/login");
+    }
+
+
+
+
+}

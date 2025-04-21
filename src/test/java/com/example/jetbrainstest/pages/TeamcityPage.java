414348b28e0061728bdc2ef4df0f2e910f9466eb
Dochkin-sergey <96723560+Dochkin-sergey@users.noreply.github.com>
2025-04-21 20:39:19 +0300

    Исправлены названия классов. Исправлен локатор по коментарию. Добавлено логирование

--- src/test/java/com/example/jetbrainstest/pages/TeamcityPage.java
+++ src/test/java/com/example/jetbrainstest/pages/TeamcityPage.java
@@ -4,26 +4,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import static com.example.jetbrainstest.tests.BaseTest.getDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TeamcityPage {
     WebDriver driver;
+    private final Logger LOG = LoggerFactory.getLogger(TeamcityPage.class);
 
-    @FindBy(xpath = "//*[contains(text(), 'Get started free')]")
+    @FindBy(xpath = "//div//div/a/span[contains(text(), 'Get started free')]")
     private WebElement ButtonStartForFree;
     @FindBy(css = "[data-test=\"site-header-profile-action\"]")
     private WebElement AuthorizationIcon;
 
-    public boolean ButtonActivityGetStartedFree (){
+    public boolean buttonActivityGetStartedFree() {
+        LOG.info("Проверка видимости текста GetStartedFree");
         return ButtonStartForFree.isEnabled();
     }
 
-    public void clickAuthorizationIcon(){
+    public void clickAuthorizationIcon() {
+        LOG.info("Клик на иконку авторизации");
         AuthorizationIcon.click();
     }
 
-    public TeamcityPage (WebDriver driver) {
+    public TeamcityPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
--- src/test/java/com/example/jetbrainstest/tests/TeamcityTest.java
+++ src/test/java/com/example/jetbrainstest/tests/TeamcityTest.java
@@ -1,16 +1,11 @@
 package com.example.jetbrainstest.tests;
 
 import com.example.jetbrainstest.MyExtension;
-import com.example.jetbrainstest.pages.AppCodePage;
 import com.example.jetbrainstest.pages.TeamcityPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -19,9 +14,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TeamcityTest extends BaseTest {
 
     private TeamcityPage teamcityPage;
+
     @BeforeEach
     @Override
-    public void setUp(){
+    public void setUp() {
         super.setUp();
         getDriver().get("https://www.jetbrains.com/teamcity/");
         teamcityPage = new TeamcityPage(getDriver());
@@ -29,14 +25,14 @@ public class TeamcityTest extends BaseTest {
 
     @Test
     @DisplayName("Проверка, что кнопка Get started free активна")
-    public void buttonGetStartedFreeActiv(){
-        assertTrue(teamcityPage.ButtonActivityGetStartedFree(), "Кнопка не активна");
+    public void buttonGetStartedFreeActiv() {
+        assertTrue(teamcityPage.buttonActivityGetStartedFree(), "Кнопка не активна");
     }
 
     @Test
     @DisplayName("Переход на страницу авторизации по иконке")
-    public void SwitchoAuthorization(){
+    public void switchoAuthorization() {
         teamcityPage.clickAuthorizationIcon();
-        assertEquals(getDriver().getCurrentUrl(), "https://account.jetbrains.com/login");
+        assertEquals("https://account.jetbrains.com/login", getDriver().getCurrentUrl());
     }
 }
